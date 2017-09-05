a84bf0c032510dfb16e108120d51536b882a9501
Admin <Admin@DESKTOP-4KU96D3>
2017-09-05 17:38:11 +0700

    add new rpn

---
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPE200Lab1
+{
+    class RPNCalculatorEngine : CalculatorEngine
+    {
+        public new string Process(string str)
+        {
+            string result = string.Empty;
+
+            Stack<string> rpnStack = new Stack<string>();
+            List<string> parts = str.Split(' ').ToList<string>();
+            foreach (string i in parts)
+            {
+                if (isNumber(i))
+                {
+                    rpnStack.Push(i);
+                }
+                if (isOperator(i))
+                {
+                    string second = rpnStack.Pop();
+                    string first = rpnStack.Pop();
+                    result = calculate(i, first, second);
+                    rpnStack.Push(result);
+                }
+            }
+
+            return result;
+        }
+    }
+}
